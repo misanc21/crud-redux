9b8eaa5f466fc921598c7a508b530db3b3da5f0b
misanc21 <misanc21@gmail.com>
2020-09-17 18:51:24 -0500

    add more html5

--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -1,11 +1,45 @@
 import React from 'react';
 
 const EditarProducto = () => {
-    return ( 
-        <h1>
-            desde edicion
-        </h1>
-     );
+    return (  
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Editar producto
+                        </h2>
+                        <form>
+                            <div className="form-group">
+                                <label>Nombre producto</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Agrega nombre de producto"
+                                    name="nombre"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Precio producto</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Agrega precio de producto"
+                                    name="precio"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >
+                                Guardar Cambios
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
  
 export default EditarProducto;
--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 const Productos = () => {
     return ( 
-        <h1>
-            desde productos 
-        </h1>
+        <Fragment>
+            <h2 className="text-center my-5"> Listado de productos</h2>
+
+            <table className="table table-striped"> 
+                <thead className="bg-primary table-dark">
+                    <tr>
+                        <th scope="col"> Nombre </th>
+                        <th scope="col"> Precio </th>
+                        <th scope="col"> Acciones </th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </Fragment>
      );
 }
  
