32bb0cc954524854cda11736552b2b9a7ca9058a
misanc21 <misanc21@gmail.com>
2020-09-17 18:44:21 -0500

    add producto form

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -2,9 +2,43 @@ import React from 'react';
 
 const NuevoProducto = () => {
     return (  
-        <h1>
-            desde nuevo prod
-        </h1>
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Agregar nuevo producto
+                        </h2>
+                        <form>
+                            <div className="form-group">
+                                <label>Nombre producto</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Agrega nombre de producto"
+                                    name="nombre"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Precio producto</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Agrega precio de producto"
+                                    name="precio"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >
+                                Agregar
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
  
